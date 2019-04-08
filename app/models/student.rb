5da42b6be31daf57d04ba1f3ab77aeecbedfaba3
Gavin Stark <gavin@gstark.com>
2019-04-08 10:49:34 -0400

    Adds active storage migrations and model changes

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,6 +1,10 @@
 class Student < ApplicationRecord
   belongs_to :cohort
 
+  # Teaches the `Student` model that it has an attached photo
+  # we can upload and show
+  has_one_attached :photo
+
   # Adding this line to tell geocoder to turn our `address` column into `latitude` and `longitude`
   geocoded_by :address
 
